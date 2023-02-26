fe9df40590d98f0665c73aff9e0c634ae9d2cc8a
Christian <cglozano97@gmail.com>
2023-02-26 21:29:03 +0100

    Documento javadoc generado

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,17 +1,35 @@
 package cuentas;
 
+/**
+ * Clase que define un objeto de tipo Cuenta
+ * @author Christian Gómez Lozano
+ *
+ */
+
 public class CCuenta {
 
-
+	/**
+	 * Atributos de la clase CCuenta
+	 */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-
+    
+    /**
+     * Constructor vacío de la clase CCuenta
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Constructor de la clase CCuenta
+     * @param nom el nombre de la cuenta
+     * @param cue el identificativo de la cuenta
+     * @param sal el saldo de la cuenta
+     * @param tipo el tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -20,6 +38,7 @@ public class CCuenta {
     }
     
     /**
+     * Devuelve atributo nombre
 	 * @return the nombre
 	 */
 	public String getNombre() {
@@ -27,6 +46,7 @@ public class CCuenta {
 	}
 
 	/**
+	 * Establece el nombre introducido como parámetro
 	 * @param nombre the nombre to set
 	 */
 	public void setNombre(String nombre) {
@@ -34,6 +54,7 @@ public class CCuenta {
 	}
 
 	/**
+	 * Devuelve el atributo cuenta
 	 * @return the cuenta
 	 */
 	public String getCuenta() {
@@ -41,6 +62,7 @@ public class CCuenta {
 	}
 
 	/**
+	 * Establece la cuenta introducida como parámetro
 	 * @param cuenta the cuenta to set
 	 */
 	public void setCuenta(String cuenta) {
@@ -48,6 +70,7 @@ public class CCuenta {
 	}
 
 	/**
+	 * Devuelve el atributo saldo
 	 * @return the saldo
 	 */
 	public double getSaldo() {
@@ -55,6 +78,7 @@ public class CCuenta {
 	}
 
 	/**
+	 * Establece el saldo introducido como parámetro
 	 * @param saldo the saldo to set
 	 */
 	public void setSaldo(double saldo) {
@@ -62,6 +86,7 @@ public class CCuenta {
 	}
 
 	/**
+	 * Devuelve el atributo interés
 	 * @return the tipoInterés
 	 */
 	public double getTipoInterés() {
@@ -69,24 +94,39 @@ public class CCuenta {
 	}
 
 	/**
+	 * Establece el tipo de interés introducido como parámetro
 	 * @param tipoInterés the tipoInterés to set
 	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
-
+	
+	/**
+	 * Devuelve el saldo
+	 * @return el saldo de la cuenta
+	 */
 	public double estado()
     {
         return getSaldo();
     }
-
+	
+	/**
+	 * Método que ingresa una cierta cantidad en la cuenta
+	 * @param cantidad a ingresar 
+	 * @throws Exception si la cantidad a ingresar es menor que 0
+	 */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Método que retira una cierta cantidad de la cuenta
+     * @param cantidad que se va a retirar
+     * @throws Exception si la cantidad a retirar es negativa o es superior al saldo actual de la cuenta
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
